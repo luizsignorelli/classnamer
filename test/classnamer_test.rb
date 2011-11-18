8a83d63e6e185378ed981570c7e8c0b29564b324
Aaron Beckerman <aaron@aaronbeckerman.com>
2011-11-18 18:13:27 +1100

    increment version to 0.0.4; switch to MiniTest, refactor test suite

--- test/classnamer_test.rb
+++ test/classnamer_test.rb
@@ -1,20 +1,12 @@
 require File.expand_path("test_helper", File.dirname(__FILE__))
 
-class ClassnamerTest < Test::Unit::TestCase
+class ClassnamerTest < MiniTest::Unit::TestCase
+  def test_all_parts_should_be_strings
+    assert Classnamer::PARTS.flatten(1).all? { |part| part.is_a?(String) }, 'all parts should be strings'
+  end
+
   def test_classnamer_should_respond_to_generate
     assert_respond_to Classnamer, :generate, 'Classnamer should respond to :generate'
-  end
-
-  def test_generate_should_not_raise
-    assert_nothing_raised('Classnamer.generate should not raise anything') do
-      Classnamer.generate
-    end
-  end
-
-  def test_generate_should_not_throw
-    assert_nothing_thrown('Classnamer.generate should not throw anything') do
-      Classnamer.generate
-    end
   end
 
   def test_generate_should_return_a_string
@@ -22,21 +14,21 @@ class ClassnamerTest < Test::Unit::TestCase
   end
 
   def test_generated_name_should_not_be_empty
-    assert Classnamer.generate.size > 0, 'generated name should not be empty'
+    refute_empty Classnamer.generate, 'generated name should not be empty'
   end
 
-  def test_generated_name_should_start_with_part_0
+  def test_generated_name_should_start_with_an_initial_part
     name = Classnamer.generate
-    assert Classnamer::PARTS[0].map { |part| (name =~ /^#{part}/).nil? }.include?(false), 'generated name should start with part 1'
+    assert Classnamer::PARTS[0].any? { |part| name =~ /^#{part}/ }, 'generated name should start with an initial part'
   end
 
-  def test_generated_name_should_contain_part_1
+  def test_generated_name_should_contain_a_middle_part
     name = Classnamer.generate
-    assert Classnamer::PARTS[1].map { |part| (name =~ /#{part}/).nil? }.include?(false), 'generated name should contain part 1'
+    assert Classnamer::PARTS[1].any? { |part| name =~ /#{part}/ }, 'generated name should contain a middle part'
   end
 
-  def test_generated_name_should_end_with_part_2
+  def test_generated_name_should_end_with_a_final_part
     name = Classnamer.generate
-    assert Classnamer::PARTS[2].map { |part| (name =~ /#{part}$/).nil? }.include?(false), 'generated name should end with part 2'
+    assert Classnamer::PARTS[2].any? { |part| name =~ /#{part}$/ }, 'generated name should end with part 2'
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,7 @@
-require 'test/unit'
+require 'rubygems'
+gem 'minitest'
+require 'minitest/unit'
+require 'minitest/autorun'
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
