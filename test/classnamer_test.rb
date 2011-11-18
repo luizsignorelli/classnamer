3d8f394d7b8698cd00181414f313c36d2c79762e
Aaron Beckerman <aaron@aaronbeckerman.com>
2011-11-19 02:30:28 +1100

    remove redundant assertion failure messages

--- test/classnamer_test.rb
+++ test/classnamer_test.rb
@@ -9,33 +9,33 @@ require 'classnamer'
 
 class ClassnamerTest < MiniTest::Unit::TestCase
   def test_all_parts_should_be_strings
-    assert Classnamer::PARTS.flatten(1).all? { |part| part.is_a?(String) }, 'all parts should be strings'
+    assert Classnamer::PARTS.flatten(1).all? { |part| part.is_a?(String) }
   end
 
   def test_classnamer_should_respond_to_generate
-    assert_respond_to Classnamer, :generate, 'Classnamer should respond to :generate'
+    assert_respond_to Classnamer, :generate
   end
 
   def test_generate_should_return_a_string
-    assert Classnamer.generate.is_a?(String), 'Classnamer.generate should return a string'
+    assert Classnamer.generate.is_a?(String)
   end
 
   def test_generated_name_should_not_be_empty
-    refute_empty Classnamer.generate, 'generated name should not be empty'
+    refute_empty Classnamer.generate
   end
 
   def test_generated_name_should_start_with_an_initial_part
     name = Classnamer.generate
-    assert Classnamer::PARTS[0].any? { |part| name =~ /^#{part}/ }, 'generated name should start with an initial part'
+    assert Classnamer::PARTS[0].any? { |part| name =~ /^#{part}/ }
   end
 
   def test_generated_name_should_contain_a_middle_part
     name = Classnamer.generate
-    assert Classnamer::PARTS[1].any? { |part| name =~ /#{part}/ }, 'generated name should contain a middle part'
+    assert Classnamer::PARTS[1].any? { |part| name =~ /#{part}/ }
   end
 
   def test_generated_name_should_end_with_a_final_part
     name = Classnamer.generate
-    assert Classnamer::PARTS[2].any? { |part| name =~ /#{part}$/ }, 'generated name should end with a final part'
+    assert Classnamer::PARTS[2].any? { |part| name =~ /#{part}$/ }
   end
 end
