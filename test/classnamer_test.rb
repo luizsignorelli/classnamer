bcd30d0e308035278cb27be49db28a2de0ded40b
Aaron Beckerman <aaron@aaronbeckerman.com>
2011-11-19 03:10:22 +1100

    reformat code

--- test/classnamer_test.rb
+++ test/classnamer_test.rb
@@ -22,8 +22,11 @@ class ClassnamerTest < MiniTest::Unit::TestCase
   def test_generated_name_should_be_in_correct_format
     name = Classnamer.generate
     refute_empty name, 'generated name should not be empty'
-    assert Classnamer::PARTS[0].any? { |part| name =~ /^#{part}/ }, 'generated name should start with an initial part'
-    assert Classnamer::PARTS[1].any? { |part| name =~ /#{part}/ }, 'generated name should contain a middle part'
-    assert Classnamer::PARTS[2].any? { |part| name =~ /#{part}$/ }, 'generated name should end with a final part'
+    assert Classnamer::PARTS[0].any? { |part| name =~ /^#{part}/ },
+      'generated name should start with an initial part'
+    assert Classnamer::PARTS[1].any? { |part| name =~ /#{part}/ },
+      'generated name should contain a middle part'
+    assert Classnamer::PARTS[2].any? { |part| name =~ /#{part}$/ },
+      'generated name should end with a final part'
   end
 end
