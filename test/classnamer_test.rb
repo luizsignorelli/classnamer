c67890eaec4eb2caa30a0b38401e25681ac6e9ad
Aaron Beckerman <aaron@aaronbeckerman.com>
2011-12-02 16:20:10 +1100

    split multi-assertion test into several tests with one assertion each

--- test/classnamer_test.rb
+++ test/classnamer_test.rb
@@ -17,14 +17,22 @@ class ClassnamerTest < MiniTest::Unit::TestCase
     assert_kind_of String, Classnamer.generate
   end
 
-  def test_generated_name_should_be_in_correct_format
+  def test_generated_name_should_not_be_empty
+    refute_empty Classnamer.generate
+  end
+
+  def test_generated_name_should_start_with_an_initial_part
     name = Classnamer.generate
-    refute_empty name, "generated name should not be empty"
-    assert Classnamer::PARTS[0].any? { |part| name =~ /^#{part}/ },
-      "generated name should start with an initial part"
-    assert Classnamer::PARTS[1].any? { |part| name =~ /#{part}/ },
-      "generated name should contain a middle part"
-    assert Classnamer::PARTS[2].any? { |part| name =~ /#{part}$/ },
-      "generated name should end with a final part"
+    assert Classnamer::PARTS[0].any? { |part| name =~ /^#{part}/ }
+  end
+
+  def test_generated_name_should_contain_a_middle_part
+    name = Classnamer.generate
+    assert Classnamer::PARTS[1].any? { |part| name =~ /#{part}/ }
+  end
+
+  def test_generated_name_should_end_with_a_final_part
+    name = Classnamer.generate
+    assert Classnamer::PARTS[2].any? { |part| name =~ /#{part}$/ }
   end
 end
