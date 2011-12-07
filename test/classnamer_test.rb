8903969eeb20229ef786358db826c2028c5c958e
Aaron Beckerman <aaron@aaronbeckerman.com>
2011-12-08 00:13:14 +1100

    add unit test that confirms that Classnamer::PART_CANDIDATE_MATRIX is being used as the default argument for Classnamer::generate

--- test/classnamer_test.rb
+++ test/classnamer_test.rb
@@ -51,4 +51,13 @@ class ClassnamerTest < MiniTest::Unit::TestCase
   def test_generate_returns_a_concatenation_of_part_candidates
     assert_equal "FooBarBaz", Classnamer.generate([["Foo"], ["Bar"], ["Baz"]])
   end
+
+  def test_generate_uses_default_part_candidate_matrix
+    seed = srand
+    srand seed
+    name1 = Classnamer::generate(Classnamer::PART_CANDIDATE_MATRIX)
+    srand seed
+    name2 = Classnamer::generate
+    assert_equal name1, name2
+  end
 end
