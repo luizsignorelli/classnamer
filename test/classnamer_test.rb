455694f52ab55030f329e629b51e12070f900364
Aaron Beckerman <aaron@aaronbeckerman.com>
2011-12-08 09:47:11 +1100

    improve variable names in tests

--- test/classnamer_test.rb
+++ test/classnamer_test.rb
@@ -18,11 +18,11 @@ class ClassnamerTest < MiniTest::Unit::TestCase
   end
 
   def test_each_part_candidate_is_a_string
-    assert Classnamer::PART_CANDIDATE_MATRIX.flatten(1).all?{|part| part.kind_of?(String)}
+    assert Classnamer::PART_CANDIDATE_MATRIX.flatten(1).all?{|part_candidate| part_candidate.kind_of?(String)}
   end
 
   def test_each_part_candidate_starts_with_an_uppercase_letter
-    assert Classnamer::PART_CANDIDATE_MATRIX.flatten(1).all?{|part| part =~ /^[A-Z]/}
+    assert Classnamer::PART_CANDIDATE_MATRIX.flatten(1).all?{|part_candidate| part_candidate =~ /^[A-Z]/}
   end
 
   def test_classnamer_responds_to_generate
@@ -35,17 +35,17 @@ class ClassnamerTest < MiniTest::Unit::TestCase
 
   def test_generated_name_starts_with_an_initial_part_candidate
     name = Classnamer.generate
-    assert Classnamer::PART_CANDIDATE_MATRIX[0].any?{|part| name =~ /^#{part}/}
+    assert Classnamer::PART_CANDIDATE_MATRIX[0].any?{|part_candidate| name =~ /^#{part_candidate}/}
   end
 
   def test_generated_name_contains_a_middle_part_candidate
     name = Classnamer.generate
-    assert Classnamer::PART_CANDIDATE_MATRIX[1].any?{|part| name =~ /#{part}/}
+    assert Classnamer::PART_CANDIDATE_MATRIX[1].any?{|part_candidate| name =~ /#{part_candidate}/}
   end
 
   def test_generated_name_ends_with_a_final_part_candidate
     name = Classnamer.generate
-    assert Classnamer::PART_CANDIDATE_MATRIX[2].any?{|part| name =~ /#{part}$/}
+    assert Classnamer::PART_CANDIDATE_MATRIX[2].any?{|part_candidate| name =~ /#{part_candidate}$/}
   end
 
   def test_generate_returns_a_concatenation_of_part_candidates
