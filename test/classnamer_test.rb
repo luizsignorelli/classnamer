86d3e7b888ae812660558cf2eea3407c148a3a30
Aaron Beckerman <aaron@aaronbeckerman.com>
2011-12-07 23:21:19 +1100

    add more unit tests; rename unit tests

--- test/classnamer_test.rb
+++ test/classnamer_test.rb
@@ -1,42 +1,50 @@
 require "test_helper"
 
 class ClassnamerTest < MiniTest::Unit::TestCase
-  def test_all_parts_should_be_strings
-    assert Classnamer::PARTS.flatten(1).all?{|part| part.kind_of?(String)}
+  def test_part_candidate_matrix_is_an_array
+    assert_kind_of Array, Classnamer::PARTS
   end
 
-  def test_all_parts_should_start_with_an_uppercase_letter
-    assert Classnamer::PARTS.flatten(1).all?{|part| part =~ /^[A-Z]/}
-  end
-
-  def test_there_should_be_three_sets_of_part_candidates
+  def test_part_candidate_matrix_has_three_elements
     assert_equal 3, Classnamer::PARTS.length
   end
 
-  def test_classnamer_should_respond_to_generate
+  def test_each_element_of_part_candidate_matrix_is_an_array
+    assert Classnamer::PARTS.all?{|part_array| part_array.kind_of?(Array)}
+  end
+
+  def test_each_part_candidate_is_a_string
+    assert Classnamer::PARTS.flatten(1).all?{|part| part.kind_of?(String)}
+  end
+
+  def test_each_part_candidate_starts_with_an_uppercase_letter
+    assert Classnamer::PARTS.flatten(1).all?{|part| part =~ /^[A-Z]/}
+  end
+
+  def test_classnamer_responds_to_generate
     assert_respond_to Classnamer, :generate
   end
 
-  def test_generate_should_return_a_string
+  def test_generate_returns_a_string
     assert_kind_of String, Classnamer.generate
   end
 
-  def test_generated_name_should_start_with_an_initial_part
+  def test_generated_name_starts_with_an_initial_part_candidate
     name = Classnamer.generate
     assert Classnamer::PARTS[0].any?{|part| name =~ /^#{part}/}
   end
 
-  def test_generated_name_should_contain_a_middle_part
+  def test_generated_name_contains_a_middle_part_candidate
     name = Classnamer.generate
     assert Classnamer::PARTS[1].any?{|part| name =~ /#{part}/}
   end
 
-  def test_generated_name_should_end_with_a_final_part
+  def test_generated_name_ends_with_a_final_part_candidate
     name = Classnamer.generate
     assert Classnamer::PARTS[2].any?{|part| name =~ /#{part}$/}
   end
 
-  def test_generate_should_return_a_concatenation_of_parts
+  def test_generate_returns_a_concatenation_of_part_candidates
     assert_equal "FooBarBaz", Classnamer.generate([["Foo"], ["Bar"], ["Baz"]])
   end
 end
