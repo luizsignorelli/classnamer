3635bcfc3aa4b0335a97d763972bc30abd91dc91
Aaron Beckerman <aaron@aaronbeckerman.com>
2012-05-12 21:04:31 +1000

    Changed to less colloquial language in comment.

--- test/classnamer_library_test.rb
+++ test/classnamer_library_test.rb
@@ -51,8 +51,8 @@ class ClassnamerLibraryTest < MiniTest::Unit::TestCase
 
   def test_generate_calls_prng_with_length_of_each_element_of_part_candidate_matrix
     matrix = [%w{A B C}, [], %w{A B}, %w{A}]
-    # I'm using a poor man's mock here because MiniTest::Mock doesn't let me
-    # mock the #call method.
+    # I'm using a homemade mock here because MiniTest::Mock doesn't let me mock
+    # the #call method.
     prng_args = []
     prng = lambda { |n|
       prng_args << n
