3c92f748465d42347a9ba0120344963ffded0bac
Aaron Beckerman <aaron@aaronbeckerman.com>
2012-10-07 14:46:04 +1100

    Code style changes

--- test/classnamer_library_test.rb
+++ test/classnamer_library_test.rb
@@ -75,21 +75,17 @@ class ClassnamerLibraryTest < Test::Unit::TestCase
   end
 
   def test_generate_raises_an_exception_when_given_an_inappropriate_matrix
-    assert_raises(NoMethodError) do
-      Classnamer.generate nil
-    end
+    assert_raises(NoMethodError) { Classnamer.generate nil }
   end
 
   def test_generate_raises_an_exception_when_given_an_inappropriate_prng
-    assert_raises(NoMethodError) do
+    assert_raises(NoMethodError) {
       Classnamer.generate Classnamer::PART_CANDIDATE_MATRIX, nil
-    end
+    }
   end
 
   def test_generate_raises_an_exception_when_argument_contains_an_inappropriate_element
-    assert_raises(NoMethodError) do
-      Classnamer.generate [["Foo"], nil]
-    end
+    assert_raises(NoMethodError) { Classnamer.generate [["Foo"], nil] }
   end
 
   def test_initialize_generator_instance
