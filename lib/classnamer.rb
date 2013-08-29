7d9f6993e74796e1bef053357999e46ff1ace738
Luiz Fernando Signorelli <luiz.sg@gmail.com>
2013-08-29 14:42:31 -0300

    Update classnamer.rb
    Adding essential words to any good class name: Process, Modern and Robot

--- lib/classnamer.rb
+++ lib/classnamer.rb
@@ -15,13 +15,13 @@ module Classnamer
        ErrorCorrecting Simple Secure Cryptographic Flexible Configurable
        Internal Cloneable Legacy Recursive Multiple Threaded Virtual Singleton
        Stateless Stateful Localized Prioritized Generic Dynamic Shared Runnable
-       Modal},
+       Modal Modern},
     %w{Byte Task Object Resource Mutex Memory List Node File Lock Pixel
        Character Command Client Server Socket Thread Notification Keystroke
        Timestamp Raster String Hash Integer Cache Scrollbar Grid Jar Connection
        Database Graph Row Column Record Metadata Transaction Message Request
        Response Query Statement Result Upload Download User Directory Button
-       Device Search Lolcat Girlfriend Yakizakana Flugzeug},
+       Device Search Lolcat Girlfriend Yakizakana Flugzeug Robot},
     %w{Sorter Allocator Tokenizer Writer Reader Randomizer Initializer Factory
        FactoryFactory Panel Frame Container Compressor Expander Counter
        Collector Collection Wrapper Accumulator Table Marshaller Demarshaller
@@ -33,7 +33,8 @@ module Classnamer
        State Processor Handler Generator Dispatcher Bundle Builder Logger
        Iterator Observer Encoder Decoder Importer Exporter Util Policy
        Preference Formatter Sequence Comparator Definition Timer Servlet
-       Controller Loader Converter Constraint Module Migrator Descriptor}
+       Controller Loader Converter Constraint Module Migrator Descriptor 
+       Process}
   ]
 
   # The default index generator, used by generate.
